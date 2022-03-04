edb11c5b2ec9f02fa56994c66ff3815c722182f0
Nikki Sabenicio <nikkiirra@gmail.com>
2022-03-04 19:00:57 +0800

    added javascript functions

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+ const statusDiv = document.querySelector('.status');
+ const restartDiv = document.querySelector('.restart');
+ const cellsDiv = document.querySelectorAll('.cell');
  
+ 
+ console.log(cellsDiv)
+ 
+
+//  console.log(restartDiv)
